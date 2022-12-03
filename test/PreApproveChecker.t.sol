e7d4ab387c72bc5e352274e9348f60f4599597e3
Vectorized <webby1111@hotmail.com>
2022-12-03 17:38:23 +0000

    Add more tests

--- test/PreApproveChecker.t.sol
+++ test/PreApproveChecker.t.sol
@@ -28,7 +28,46 @@ contract PreApproveCheckerTest is PreApproveRegistryVanityTest {
             assertEq(PreApproveChecker.isPreApproved(v.operator, v.collector, v.lister), true);
 
             uint256 gasBefore = gasleft();
-            registry.isPreApproved(v.operator, v.collector, v.lister);
+            PreApproveChecker.isPreApproved(v.operator, v.collector, v.lister);
+            console.log(gasBefore - gasleft());
+
+            vm.warp(block.timestamp + _random() % 8);
+
+            if (_random() % 2 == 0) {
+                vm.prank(v.lister);
+                registry.removeOperator(v.operator);
+                assertEq(registry.startTime(v.lister, v.operator), 0);
+            }
+        }
+    }
+
+    function testCheckIsPreApprovedWithoutRegistry(uint256) public {
+        PreApproveRegistryTest.setUp();
+        vm.etch(PreApproveChecker.PRE_APPROVE_REGISTRY, bytes(""));
+
+        TestVars memory v = _testVars(1);
+        assertEq(PreApproveChecker.isPreApproved(v.operator, v.collector, v.lister), false);
+
+        vm.prank(v.collector);
+        registry.subscribe(v.lister);
+
+        for (uint256 t; t != 2; ++t) {
+            vm.prank(v.lister);
+            registry.addOperator(v.operator);
+            assertEq(PreApproveChecker.isPreApproved(v.operator, v.collector, v.lister), false);
+
+            uint256 begins = registry.startTime(v.lister, v.operator);
+            vm.warp(begins - 1);
+            assertEq(PreApproveChecker.isPreApproved(v.operator, v.collector, v.lister), false);
+
+            vm.warp(begins);
+            assertEq(PreApproveChecker.isPreApproved(v.operator, v.collector, v.lister), false);
+
+            vm.warp(begins + _random() % 256);
+            assertEq(PreApproveChecker.isPreApproved(v.operator, v.collector, v.lister), false);
+
+            uint256 gasBefore = gasleft();
+            PreApproveChecker.isPreApproved(v.operator, v.collector, v.lister);
             console.log(gasBefore - gasleft());
 
             vm.warp(block.timestamp + _random() % 8);
